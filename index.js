0a0b5abbc8064ce7889003d7457c7e8225334ba6
Duy Nguyen <duy210697@gmail.com>
2021-06-11 09:39:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function count(str) {
+  let arr = str.match(/(\d)\1*/g);
+
+  return arr.reduce((res, item) => {
+    return res + `${item.length}` + `${item[0]}`;
+  }, "");
+}
+
+function countAndSay(n) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    if (i == 0) {
+      str = "1";
+    } else {
+      str = count(str);
+    }
+  }
+
+  return str;
+}
+
+console.log(countAndSay(4));
+// console.log("res:", count("11"))
