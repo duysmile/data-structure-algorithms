b74b92622d38025a755b7db22b08bc320dcc2052
Duy Nguyen <duy210697@gmail.com>
2021-06-14 10:02:03 +0000

    update O(n) for min jump problem

--- min-jumps.js
+++ min-jumps.js
@@ -5,6 +5,8 @@ Input: nums = [2,3,1,1,4]
 Output: 2 (Chỉ cần nhảy 2 bước, bước đầu tiên từ index 0 tới index 1, sau đó nhảy tới vị trí cuối cùng)
 */
 
+// O(n^2)
+
 function calMinJump(arr, index) {
   let min = arr.length;
   
@@ -23,6 +25,31 @@ function calMinJump(arr, index) {
   return min;
 }
 
-const arr = [2,3,1,1,4];
+const arr = [4,1,2,3,3,2,4,6,1,1,1,1,1,1,1]; // 4
 
-console.log(calMinJump(arr, 0));
+// console.log(calMinJump(arr, 0));
+
+// O(n)
+// Solution: https://stackoverflow.com/questions/33227584/shortest-path-through-an-array-of-max-jump-lengths?rq=1
+
+function calMinJumpV2(arr) {
+  let count = 0;
+  let i = 0;
+  while (i < arr.length - 1) {
+    let jump = arr[i] + i;
+    let maxJump = 0;
+    let index = i;
+    for (let j = i + 1; j <= jump; j++) {
+      if (maxJump <= arr[j] + j) {
+        maxJump = arr[j] + j;
+        index = j;
+      }
+    }
+    i = index;
+    count += 1;
+  }
+
+  return count;
+}
+
+console.log(calMinJumpV2(arr));
